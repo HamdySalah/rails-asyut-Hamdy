90a0f61b39f190294dbd59bfa834b3e42ac3e626
Hamdy Salah <hamdysalah.dev@gmail.com>
2025-05-27 05:42:37 +0300

    added new column addres to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_27_022651) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_27_024109) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
@@ -25,5 +25,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_27_022651) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address", null: false
   end
 end
